2e8dcd6bf6139ed05636a078d7da367e9ac49c14
Janusz <gleaken@gmail.com>
2020-10-11 17:02:31 +0200

    tests

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,5 +1,17 @@
 import hello
 
 
-def test_says_world():
+def test_says_wor1ld():
     assert hello.say_what() == 'wor1ld'
+
+def test_say_world():
+    assert hello.say_what() == 'world'
+
+def test_dummy1():
+    assert 'world' == 'world'
+
+def test_dummy2():
+    assert '1' == '1'
+
+def test_dummy3():
+    assert '2' == '2'
